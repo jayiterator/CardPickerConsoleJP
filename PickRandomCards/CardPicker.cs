92efcf3242b4ecbc4882ca1f8724154fc0f82eb9
jayiterator <jayson.nm.paul@proton.me>
2023-07-22 10:56:31 +1000

    Update 1

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace PickRandomCards
+{
+    public class CardPicker
+    {
+
+        static Random random = new Random();
+
+        public CardPicker()
+        {
+        }
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            //if (value == 1) return "Spades";
+            //if (value == 2) return "Spades";
+            //if (value == 3) return "Spades";
+            //return "Diamonds";
+
+
+            switch (value)
+            {
+                case 1:
+                    return "Spades";
+                case 2:
+                    return "Hearts";
+                case 3:
+                    return "Clubs";
+                default:
+                    return "Diamonds";
+            }
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            switch (value)
+            {
+                case 1:
+                    return "Ace";
+                case 11:
+                    return "Jack";
+                case 12:
+                    return "Queen";
+                case 13:
+                    return "King";
+                default:
+                    return value.ToString();
+            }
+        }
+    }
+}
+
--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
 
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                string[] results = CardPicker.PickSomeCards(numberOfCards);
+
+                foreach (string result in results)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input not a valid number.");
+            }
+
+
+        }
+    }
+}
